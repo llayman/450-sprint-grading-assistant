152b94e924731a806557cb28229488c3940d7f54
Lucas Layman <laymanl@uncw.edu>
2022-10-21 15:59:23 -0400

    Added PR info

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ import json
 
 from collections import namedtuple
 from github import Github
+from typing import Dict
 
 Sprint = namedtuple('Sprint', 'start end')
 SPRINT_1 = Sprint(datetime(2022, 10, 7, tzinfo=ZoneInfo('US/Eastern')),
                   datetime(2022, 10, 20, tzinfo=ZoneInfo('US/Eastern')))
 
 ORG = "UNCW-CSC-450"
+
+
+class UserStats:
+
+    def __init__(self):
+        self.pulls = []
+        self.commits = []
+
+
 if __name__ == "__main__":
 
     with open("token.json", "r") as token_file:
@@ -32,23 +42,32 @@ if __name__ == "__main__":
         r = org.get_repo(repo)
         print(r.full_name)
 
-        member_commits = {}
-        for c in r.get_commits(since=SPRINT_1.start, until=SPRINT_1.end):
-            if c.author.login not in member_commits:
-                member_commits[c.author.login] = []
-            member_commits[c.author.login].append(c)
-
+        user_stats: Dict[str, UserStats] = {}
         # TODO: Count pull requests
 
-        for author, commits in member_commits.items():
-            print(f"\n\n{author} - {len(commits)} commits")
-            total = 0
-            for c in commits:
+        for c in r.get_commits(since=SPRINT_1.start, until=SPRINT_1.end):
+            if c.author.login not in user_stats:
+                user_stats[c.author.login] = UserStats()
+            user_stats[c.author.login].commits.append(c)
+
+        for p in r.get_pulls(state="all"):
+            user_stats[p.user.login].pulls.append(p)
+
+
+
+
+        for author, stats in user_stats.items():
+            print(f"\n\n{author} - {len(stats.commits)} commits, {len(stats.pulls)} PRs")
+            print(f'\tCommits: {len(stats.commits)}')
+            for c in stats.commits:
                 # the commit's last_modified is when it was merged into main
                 # the commitStats last_modified is when the source files were last worked on
                 # format is Mon, 10 Oct 2022 21:33:08 GMT
                 # last_mod = c.stats.last_modified
                 # formatted = datetime.strptime(last_mod, "%a, %d %b %Y %X %Z")
-                print(f"\t{c.stats.last_modified} {len(c.files)} files, total:{c.stats.total} adds:{c.stats.additions} "
+                print(f"\t\t{c.stats.last_modified} {len(c.files)} files, total:{c.stats.total} adds:{c.stats.additions} "
                       f"deletes:{c.stats.deletions} https://github.com/{r.full_name}/commit/{c.url.split('/')[-1]}")
+            print(f'\tPRs:{len(stats.pulls)}')
+            for p in stats.pulls:
+                print(f"\t\t{p.created_at} {p.html_url}")
         exit()
